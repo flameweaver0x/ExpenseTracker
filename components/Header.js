235c6264f126d0242b21a546c5fed57f19cf2c9f
flameweaver0x <163330032+flameweaver0x@users.noreply.github.com>
2024-06-16 20:26:53 +0200

    Push Header.js

---
+++ components/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <nav className="navbar">
+                <div className="logo">ExpenseTracker</div>
+                <ul className="nav-links">
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/expenses">Expenses</Link>
+                    </li>
+                    <li>
+                        <Link to="/report">Report</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
